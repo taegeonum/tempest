142e02bafc6a1c0b3676f699f2682ac98b2f0787
taegeonum <taegeonum@gmail.com>
2014-11-17 20:43:02 +0900

    Local test version of mtss and naive

--- src/main/java/edu/snu/org/WordcountApp.java
+++ src/main/java/edu/snu/org/WordcountApp.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import edu.snu.org.mtss.MTSSTopologyRunner;
 import org.apache.log4j.Logger;
 
 import backtype.storm.Config;
+import edu.snu.org.mtss.MTSSTopologyRunner;
 import edu.snu.org.mtss.Timescale;
 import edu.snu.org.naive.NaiveWordCountRunner;
 
 public class WordcountApp {
 
   private static final Logger LOG = Logger.getLogger(WordcountApp.class);
-  private static final int DEFAULT_RUNTIME_IN_SECONDS = 12000;
+  private static final int DEFAULT_RUNTIME_IN_SECONDS = 5 * 60;
   private static final int TOP_N = 10;
   
-  private static final int NUM_SPOUT = 16;
+  private static final int NUM_SPOUT = 8;
   private static final int NUM_WC_BOLT = 4;
   
-  private static final int INPUT_INTERVAL = 1;
+  private static final int INPUT_INTERVAL = 5;
   private static final int NUM_WORKERS = 1;
   
   private final String topologyName;
@@ -54,17 +54,29 @@ public class WordcountApp {
       runLocally = false;
     }
 
+    List<Timescale> timescales = new ArrayList<>();
+    timescales.add(new Timescale(30, 2, TimeUnit.SECONDS, TimeUnit.SECONDS));
+    timescales.add(new Timescale(60, 5, TimeUnit.SECONDS, TimeUnit.SECONDS));
+    timescales.add(new Timescale(90, 8, TimeUnit.SECONDS, TimeUnit.SECONDS));
+    timescales.add(new Timescale(120, 10, TimeUnit.SECONDS, TimeUnit.SECONDS));
+    
     LOG.info("Topology name: " + topologyName);
     runner = new MTSSTopologyRunner();
     
-    List<Timescale> timescales = new ArrayList<>();
-    timescales.add(new Timescale(2, 1, TimeUnit.SECONDS, TimeUnit.SECONDS));
-    timescales.add(new Timescale(4, 2, TimeUnit.SECONDS, TimeUnit.SECONDS));
-    timescales.add(new Timescale(8, 4, TimeUnit.SECONDS, TimeUnit.SECONDS));
-    timescales.add(new Timescale(16, 8, TimeUnit.SECONDS, TimeUnit.SECONDS));
-    timescales.add(new Timescale(32, 16, TimeUnit.SECONDS, TimeUnit.SECONDS));
-    timescales.add(new Timescale(64, 32, TimeUnit.SECONDS, TimeUnit.SECONDS));
-    timescales.add(new Timescale(128, 64, TimeUnit.SECONDS, TimeUnit.SECONDS));
+    
+    if (runLocally) {
+      LOG.info("Running in local mode");
+      runner.runLocally(createTopologyConfiguration(), NUM_SPOUT, NUM_WC_BOLT, TOP_N, timescales, DEFAULT_RUNTIME_IN_SECONDS, INPUT_INTERVAL);
+    }
+    else {
+      LOG.info("Running in remote (cluster) mode");
+      runner.runRemotely(createTopologyConfiguration(), NUM_SPOUT, NUM_WC_BOLT, TOP_N, timescales, DEFAULT_RUNTIME_IN_SECONDS, INPUT_INTERVAL);
+    }
+    
+    Thread.sleep(1 * 60 * 1000);
+    
+    runner = new NaiveWordCountRunner();
+    
     
     if (runLocally) {
       LOG.info("Running in local mode");
