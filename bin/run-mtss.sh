e63fd0d8db1d72e6de7164183b952c61f9da79de
taegeonum <taegeonum@gmail.com>
2014-11-19 17:25:41 +0900

    Update run.sh

--- bin/run-mtss.sh
+++ bin/run-mtss.sh
@@ -3,7 +3,7 @@
 #$CMD
 
 SELF_JAR='./target/mtss-1.0-SNAPSHOT-jar-with-dependencies.jar'
-TARGET_CLASS='edu.snu.org.WordcountApp'
+TARGET_CLASS='edu.snu.org.WordcountAppMTSS'
 
 CMD="java -cp ::$SELF_JAR $LOCAL_RUNTIME_TMP $LOGGING_CONFIG $TARGET_CLASS $*"
 echo $CMD
--- bin/run-naive.sh
+++ bin/run-naive.sh
@@ -3,7 +3,7 @@ echo $CMD
 $CMD
 
 SELF_JAR='./target/mtss-1.0-SNAPSHOT-jar-with-dependencies.jar'
-TARGET_CLASS='edu.snu.org.naive.NaiveWordCountTopology'
+TARGET_CLASS='edu.snu.org.WordcountAppNaive'
 
 CMD="java -cp ::$SELF_JAR $LOCAL_RUNTIME_TMP $LOGGING_CONFIG $TARGET_CLASS $*"
 echo $CMD
