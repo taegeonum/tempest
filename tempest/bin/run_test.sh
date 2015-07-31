0225d29f9f2d1da608128de37af0b229344fb3c4
taegeonum <taegeonum@gmail.com>
2015-07-31 17:04:28 +0900

    remove mtss

--- tempest/bin/run_test.sh
+++ tempest/bin/run_test.sh
@@ -3,7 +3,7 @@
 # run WordCount test topology
 # parameter: a file path containing test parameters
 
-SELF_JAR='./target/mtss-0.11-SNAPSHOT.jar'
+SELF_JAR='./target/tempest-0.11-SNAPSHOT.jar'
 TARGET_CLASS='edu.snu.tempest.example.storm.wordcount.MTSWordCountTestTopology'
 
 CMD="java -cp ::$SELF_JAR $LOCAL_RUNTIME_TMP $LOGGING_CONFIG $TARGET_CLASS `cat $1`"
