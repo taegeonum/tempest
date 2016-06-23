063b6e689c5456de8e6fae432a1d833daa53e5cd
taegeonum <taegeonum@gmail.com>
2016-06-23 16:20:06 +0900

    default endtime

--- src/main/java/vldb/evaluation/parameter/EndTime.java
+++ src/main/java/vldb/evaluation/parameter/EndTime.java
@@ -6,6 +6,6 @@ import org.apache.reef.tang.annotations.NamedParameter;
 /**
  * Created by taegeonum on 4/29/16.
  */
-@NamedParameter(short_name = "total_time")
+@NamedParameter(short_name = "total_time", default_value = "0")
 public class EndTime implements Name<Long> {
 }
