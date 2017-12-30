e9a51054189cd72053a403f5b7066ceb673b7179
taegeonum <taegeonum@gmail.com>
2017-12-30 17:36:28 +0900

    add ts path

--- src/main/java/vldb/evaluation/TwitterEvaluation.java
+++ src/main/java/vldb/evaluation/TwitterEvaluation.java
@@ -15,6 +15,8 @@ import vldb.operator.window.timescale.Timescale;
 import vldb.operator.window.timescale.common.TimescaleParser;
 import vldb.operator.window.timescale.parameter.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
@@ -35,7 +37,8 @@ public final class TwitterEvaluation {
 
     final CommandLine cl = new CommandLine(cb)
         .registerShortNameOfClass(OperatorTypeParam.class)
-        .registerShortNameOfClass(TimescaleString.class)
+        //.registerShortNameOfClass(TimescaleString.class)
+        .registerShortNameOfClass(TimescalePath.class)
         .registerShortNameOfClass(OutputPath.class)
         .registerShortNameOfClass(FileWordGenerator.FileDataPath.class)
         .registerShortNameOfClass(NumThreads.class)
@@ -54,6 +57,10 @@ public final class TwitterEvaluation {
     return cl.getBuilder().build();
   }
 
+  private static String getTimescaleString(final String timescalePath) throws IOException {
+    final BufferedReader br = new BufferedReader(new FileReader(timescalePath));
+    return br.readLine();
+  }
 
   public static void main(final String[] args) throws Exception {
 
@@ -61,7 +68,8 @@ public final class TwitterEvaluation {
     final Injector injector = Tang.Factory.getTang().newInjector(commandLineConf);
 
     // Parameters
-    final String timescaleString = injector.getNamedInstance(TimescaleString.class);
+    final String timescalePath = injector.getNamedInstance(TimescalePath.class);
+    final String timescaleString = getTimescaleString(timescalePath);
      String outputPath = injector.getNamedInstance(OutputPath.class);
     final List<Timescale> timescales = TimescaleParser.parseFromString(timescaleString);
     final int numThreads = injector.getNamedInstance(NumThreads.class);
@@ -75,7 +83,6 @@ public final class TwitterEvaluation {
     final int windowGap = injector.getNamedInstance(WindowGap.class);
     final int sharedFinalNum = injector.getNamedInstance(SharedFinalNum.class);
     final double overlapRatio = injector.getNamedInstance(OverlappingRatio.class);
-    //final long numKey = injector.getNamedInstance(NumOfKey.class);
 
     final TestRunner.OperatorType operatorType = TestRunner.OperatorType.valueOf(
         injector.getNamedInstance(OperatorTypeParam.class));
